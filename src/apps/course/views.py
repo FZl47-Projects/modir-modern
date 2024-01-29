f7e4752852bd3945aa30e3ebb16d5c8d21d82977
AmirMahdi Azarboon <amirazarboon@gmail.com>
2024-01-29 15:30:47 +0330

    Modify: add static banner in index page

--- src/apps/course/views.py
+++ src/apps/course/views.py
@@ -1,7 +1,6 @@
 from django.views.generic import View, TemplateView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.translation import gettext as _
-from django.shortcuts import redirect, reverse
 from django.contrib import messages
 
 from .models import Course
