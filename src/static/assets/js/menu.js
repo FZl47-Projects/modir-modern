b9ef85b93d250452aeb97ca15ab23c0b0b3772de
AmirMahdi Azarboon <amirazarboon@gmail.com>
2024-01-31 18:54:25 +0330

    Modify: menu toggle btn

--- src/static/assets/js/menu.js
+++ src/static/assets/js/menu.js
@@ -1,22 +1,20 @@
 // Move menubar on click
 const toggleBtn = document.querySelector('.menu-toggle-btn');
+const toggleCloseBtn = document.querySelector('.menu-toggle-btn-close');
 const sidebar = document.querySelector('.menu-fixed-right');
 
 toggleBtn.addEventListener('click', function () {
-    if (toggleBtn.style.right === '250px') {
-        toggleBtn.style.right = '0';
-        sidebar.style.right = '-300px';
-    } else {
-        toggleBtn.style.right = '250px';
-        sidebar.style.right = '0';
-    }
+    sidebar.style.right = '0';
+})
+
+toggleCloseBtn.addEventListener('click', function () {
+    sidebar.style.right = '-300px';
 })
 
 let computedStyle = window.getComputedStyle(toggleBtn);
 if (computedStyle.getPropertyValue('display') !== 'none') {
     document.onclick = function (e) {
         if (!sidebar.contains(e.target) && !e.target.classList.contains('menu')) {
-            toggleBtn.style.right = '0';
             sidebar.style.right = '-300px';
         }
     }
