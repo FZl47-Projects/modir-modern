c43f92d9f090b87f6c78211f663a561d102061f8
AmirMahdi Azarboon <amirazarboon@gmail.com>
2024-02-13 22:54:46 +0330

    Add: subscription end warn notif type

--- src/apps/notification/enums.py
+++ src/apps/notification/enums.py
@@ -5,3 +5,4 @@ from django.db.models import TextChoices
 # Notification Type choices
 class NotificationTypes(TextChoices):
     MOBILE_VERIFICATION_CODE = 'MOBILE_VERIFICATION_CODE', _('Mobile verification code')
+    SUBSCRIPTION_END_WARNING = 'SUBSCRIPTION_END_WARNING', _('Subscription end warning')
--- src/apps/notification/sms.py
+++ src/apps/notification/sms.py
@@ -8,9 +8,13 @@ class NotificationUser:
         pattern = 'h63qzke49mrw4aq'
         send_sms(phone_number, pattern, code=notification.kwargs['code'])
 
-    # TODO: Add more sms handlers for other notifications
+    @classmethod
+    def subscription_end_warning(cls, notification, phone_number):
+        pattern = None  # TODO: Add pattern code from ippanel
+        send_sms(phone_number, pattern, days=notification.kwargs['days'])
 
 
 NOTIFICATION_USER_HANDLERS = {
     'MOBILE_VERIFICATION_CODE': NotificationUser.mobile_verification_code_handler,
+    'SUBSCRIPTION_END_WARNING': NotificationUser.subscription_end_warning,
 }
