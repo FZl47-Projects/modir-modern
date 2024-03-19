dd9f02771b263c8096b76bd8a93c9b4f40c57745
AmirMahdi Azarboon <amirazarboon@gmail.com>
2024-03-20 02:31:14 +0330

    Modify: change notification model admin

--- src/apps/notification/admin.py
+++ src/apps/notification/admin.py
@@ -5,6 +5,8 @@ from .models import Notification
 # Register Notification admin
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'to_user', 'type', 'send_notify')
+    list_display = ('id', 'to_user', 'type', 'send_notify', 'created_at')
     list_display_links = ('id', 'to_user')
     list_filter = ('send_notify', 'is_showing', 'type')
+    search_fields = ('to_user__phone_number',)
+    readonly_fields = ('created_at', 'updated_at')
