418615fbf8c92721dfdd58eb8a65e15203e15281
Fazel Momeni <wwwfazel88@gmail.com>
2024-08-21 06:54:36 +0330

    set required and optional fields

--- src/apps/customers/models.py
+++ src/apps/customers/models.py
@@ -37,7 +37,7 @@ class Counseling(models.Model):
     user = models.ForeignKey('account.User', on_delete=models.CASCADE, verbose_name=_('User'))
     field_of_activity = models.CharField(max_length=25, choices=FIELDS.choices, verbose_name=_('Field of Activity'))
     work_experience = models.CharField(max_length=20, verbose_name=_('Work Experience'))
-    work_shift = models.CharField(max_length=50, verbose_name=_('Work Shift'))
+    work_shift = models.CharField(max_length=50, verbose_name=_('Work Shift'), null=True, blank=True)
     number_of_staff = models.IntegerField(_('Number of staff'))
     capacity = models.IntegerField(_('Capacity'))
     personnel_training = models.TextField(null=True, blank=True, verbose_name=_('Personnel Training Detail'))
@@ -47,19 +47,19 @@ class Counseling(models.Model):
                                                         verbose_name=_('Conduct Inventory Reconciliation'))
 
     has_control_cost_system = models.CharField(max_length=5, choices=CONTROL_COST_SYSTEM.choices,
-                                               verbose_name=_('Has Control Cost System'))
+                                               verbose_name=_('Has Control Cost System'), null=True, blank=True)
 
     has_book_of_regulations = models.CharField(max_length=5, choices=BOOK_OF_REGULATIONS.choices,
-                                               verbose_name=_('Has Book of Regulations'))
+                                               verbose_name=_('Has Book of Regulations'), null=True, blank=True)
 
     has_tasks_written = models.CharField(max_length=5, choices=TASKS_WRITTEN.choices,
-                                         verbose_name=_('Has Tasks Written'))
+                                         verbose_name=_('Has Tasks Written'), null=True, blank=True)
 
     has_recipes_written = models.CharField(max_length=5, choices=RECIPES_WRITTEN.choices,
-                                           verbose_name=_('Has Recipes Written'))
+                                           verbose_name=_('Has Recipes Written'), null=True, blank=True)
 
     has_instructions_written = models.CharField(max_length=5, choices=INSTRUCTION_WRITTEN.choices,
-                                                verbose_name=_('Has Instructions Written'))
+                                                verbose_name=_('Has Instructions Written'), null=True, blank=True)
 
     class Meta:
         verbose_name = _('Counseling')
